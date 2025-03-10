f4e1b6a862007a8e981a701cc5a60808b2255fda
Yimkeul <leesjun29@gmail.com>
2025-03-10 14:33:15 +0900

    feat: 3자리수를 입력하고 힌트를 받기 구현
    checkError, checkHint 구현

---
+++ LSJBaseBallGame/LSJBaseBallGame/Data/Hint.swift
@@ -0,0 +1,18 @@
+//
+//  Hint.swift
+//  LSJBaseBallGame
+//
+//  Created by yimkeul on 3/10/25.
+//
+
+import Foundation
+
+struct Hint {
+    var strikes: Int
+    var balls: Int
+
+    init() {
+        self.strikes = 0
+        self.balls = 0
+    }
+}
