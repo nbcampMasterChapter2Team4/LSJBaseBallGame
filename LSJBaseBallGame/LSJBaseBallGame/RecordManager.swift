aa67c6a6667966f453f75d33c2de2b55201ad1f9
Yimkeul <leesjun29@gmail.com>
2025-03-10 15:49:40 +0900

    feat: 게임 기록 보기 기능 추가

---
+++ LSJBaseBallGame/LSJBaseBallGame/RecordManager.swift
@@ -0,0 +1,38 @@
+//
+//  RecordManager.swift
+//  LSJBaseBallGame
+//
+//  Created by yimkeul on 3/10/25.
+//
+
+import Foundation
+
+class RecordManager {
+    static let shared = RecordManager()
+    
+    var tryCountHistory: [Int]
+    
+    private init() {
+        tryCountHistory = []
+    }
+    
+    // Lv5: 경기 횟수 기록하기
+    func saveCount(_ tryCount: Int) {
+        self.tryCountHistory.append(tryCount)
+    }
+    
+    // Lv5: 경기 횟수 출력하기
+    func showHistory() {
+        print(MessageConstants.showHistoryMessage)
+        if tryCountHistory.isEmpty {
+            print(MessageConstants.emtpyHistoryMessage)
+        } else {
+            for (i,history) in tryCountHistory.enumerated() {
+                print("\(i+1)번째 게임 : 시도 횟수 - \(history)")
+            }
+        }
+        print(MessageConstants.lineSpace)
+        
+    }
+    
+}
